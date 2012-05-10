3f3c72a061598d04d963ae0590958b6cd5e37d54
Jirat K <imn3tr@gmail.com>
2012-05-10 22:03:47 +0700

    Initial Commit

---
+++ Zerber/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Zerber
+//
+//  Created by n3tr on 5/10/55 BE.
+//  Copyright (c) 2555 Simpletail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Zerber/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Zerber
+//
+//  Created by n3tr on 5/10/55 BE.
+//  Copyright (c) 2555 Simpletail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
